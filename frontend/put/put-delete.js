eeb00bd4c1e5b451e70efeb78c5754048f6b13f3
Helviley Lacerda <helviley.melo@gmail.com>
2022-12-01 14:31:04 -0300

    Repositório com projetos back e front end

--- frontend/put/put-delete.js
+++ frontend/put/put-delete.js
@@ -2,8 +2,6 @@ const exps = "express";
 const url = "http://localhost:3000/account/list";
 const URLedit = "http://localhost:3000/account/put/";
 const URLdelete = "http://localhost:3000/account/delete/";
-let editButton;
-let deleteButton;
 
 function userData() {
   fetch(url, {
@@ -15,62 +13,69 @@ function userData() {
     .then((response) => response.json())
     .then((json) => {
       let data = document.querySelector("table > tbody");
-      let ul;
-      let li;
+
+      ul_html = ``;
       for (const id in json) {
-        ul = document.createElement("ul");
         for (const info in json[id]) {
-          li = document.createElement("li");
-          editButton = document.createElement("button");
-          editButton.id = `editButton_${id}`;
-          var lbl = document.createTextNode("Editar");
-          editButton.appendChild(lbl);
-          deleteButton = document.createElement("button");
-          deleteButton.id = `deleteButton_${id}`;
-          var lb = document.createTextNode("Deletar");
-          deleteButton.appendChild(lb);
-          li.innerHTML = `${info}: ${json[id][info]}`;
-          ul.appendChild(li);
+          ul_html += `
+              <li>${info}: ${json[id][info]}</li>
+          `;
         }
-        data.appendChild(ul);
-        data.appendChild(editButton);
-        data.appendChild(deleteButton);
+        ul_html += `
+          <button id="editButton_${id}" onclick=editData("${id}")>Editar</button>
+          <button id="deleteButton_${id}" onclick=Confirm("${id}")>Deletar</button>
+      `;
       }
+      data.innerHTML = ul_html;
     })
     .catch((error) => {
       console.log(error);
     });
 }
-
-function OnClick() {
-  editButton.onclick = editData("${id}");
-  deleteButton.onclick = deleteData("${id}");
+const swicthModal = () => {
+  const modal = document.querySelector(".form");
+  const actualStyle = modal.style.display
+  if(actualStyle == "block"){
+    modal.style.display = "none"
+  }else{
+    modal.style.display = "block"
+  }
 }
 
-function editData() {
+function editData(id) {
   // const Window = window.open("put-delete.html", "popup")
-  fetch(URLedit, {
+  fetch(URLedit + id, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
   })
-    .then(console.log("teste editar"))
-    .then()
+    .then((response) => response.json())
     .catch((error) => {
       console.log(error);
     });
 }
 
-function deleteData() {
-  fetch(URLdelete, {
+function Confirm(id){
+  var conf;
+  var c = confirm("Ao seguir com a opção os dados serão apagados permanentemente. Você tem certeza que deseja deletar os dados?");
+  if(c == true){
+    conf = deleteData(id);
+    console.log("Dados excluídos!")
+  }else{
+    conf = "Operação cancelada!"
+    console.log("Operação cancelada!")
+  }
+}
+
+function deleteData(id){
+  fetch(URLdelete + id, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
   })
-    .then(console.log("teste deletar"))
-    .then()
+    .then((response) => response.json())
     .catch((error) => {
       console.log(error);
     });
