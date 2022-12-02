bef3f269387d686bf58f88bd077c4e36b6d43775
Helviley Lacerda <helviley.melo@gmail.com>
2022-12-02 14:25:22 -0300

    Repositório com projetos back e front end

--- frontend/put/put-delete.js
+++ frontend/put/put-delete.js
@@ -1,7 +1,7 @@
 const exps = "express";
 const url = "http://localhost:3000/account/list";
-const URLedit = "http://localhost:3000/account/put/";
 const URLdelete = "http://localhost:3000/account/delete/";
+const URLedit = "http://localhost:3000/account/put/";
 
 function userData() {
   fetch(url, {
@@ -22,8 +22,12 @@ function userData() {
           `;
         }
         ul_html += `
-          <button id="editButton_${id}" onclick=editData("${id}")>Editar</button>
-          <button id="deleteButton_${id}" onclick=Confirm("${id}")>Deletar</button>
+          <button
+          style= "background: linear-gradient(-8deg, #27b53a, #fff)", background: linear-gradient(8deg, #fff, #00ff22) 
+          class="editButton_${id}" 
+          onclick=Modal("${id}")>Editar</button>
+          
+          <button class="deleteButton_${id}" onclick=Confirm("${id}")>Deletar</button>
       `;
       }
       data.innerHTML = ul_html;
@@ -32,39 +36,86 @@ function userData() {
       console.log(error);
     });
 }
-const swicthModal = () => {
-  const modal = document.querySelector(".form");
-  const actualStyle = modal.style.display
-  if(actualStyle == "block"){
-    modal.style.display = "none"
-  }else{
-    modal.style.display = "block"
-  }
+
+function Modal(id){
+  let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  modal.addEventListener("click", (e) => {
+    if(e.target.className == "button-submit"){
+      editData(id);
+      console.log("TESTANDO ID: " , id);
+      form.reset();
+    }
+  })
 }
 
 function editData(id) {
-  // const Window = window.open("put-delete.html", "popup")
   fetch(URLedit + id, {
+    // console.log("TESTANDO ID: " + id)
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
   })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.log(error);
+   .then((response) => response.json())
+   .catch((error) => {
+     console.log(error);
     });
 }
+
+function closeModal(myModal){
+  let modal = document.getElementById("myModal");
+  modal.classList.add("mostrar")
+  modal.addEventListener("click", (e) => {
+    if(e.target.id == myModal || e.target.className == "button-cancell"){
+      modal.classList.remove("mostrar");
+    }
+  })
+  modal.style.display = "none";
+  Window = Window.close
+  document.body.style.overflow = "auto";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function Confirm(id){
   var conf;
   var c = confirm("Ao seguir com a opção os dados serão apagados permanentemente. Você tem certeza que deseja deletar os dados?");
   if(c == true){
     conf = deleteData(id);
-    console.log("Dados excluídos!")
   }else{
     conf = "Operação cancelada!"
-    console.log("Operação cancelada!")
   }
 }
 
