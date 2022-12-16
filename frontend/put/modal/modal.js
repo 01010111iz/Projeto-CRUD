47a66c6d55c7362535fd69ca4532fc5ca74fd2f3
Helviley Lacerda <helviley.melo@gmail.com>
2022-12-16 10:55:20 -0300

    Repositório com projetos back e front end

--- frontend/put/modal/modal.js
+++ frontend/put/modal/modal.js
@@ -2,12 +2,21 @@ function dataModal(id) {
     let name = document.querySelector("#name_" + id).innerText;
     let rg = document.querySelector("#rg_" + id).innerText;
     let cpf = document.querySelector("#cpf_" + id).innerText;
-    let address = document.querySelector("#address_" + id).innerText;
+    let cep = document.querySelector("#cep_" + id).innerText;
+    let street = document.querySelector("#street_" + id).innerText;
+    let number = document.querySelector("#number_" + id).innerText;
+    let district = document.querySelector("#district_" + id).innerText;
+    let city = document.querySelector("#city_" + id).innerText;
+
   
     document.getElementById("nameModal").value = name;
     document.getElementById("RGModal").value = rg;
     document.getElementById("CPFModal").value = cpf;
-    document.getElementById("addressModal").value = address;
+    document.getElementById("cepModal").value = cep;
+    document.getElementById("streetModal").value = street;
+    document.getElementById("numberModal").value = number;
+    document.getElementById("districtModal").value = district;
+    document.getElementById("cityModal").value = city;
   }
   
   function Modal(id) {
